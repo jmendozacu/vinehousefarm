54966d8be211d1210dfe0f7a28b14b73423ead32
Tim Airey <timairey87@googlemail.com>
2016-03-10 09:34:02 +0000

    Readded bird name to page title.

--- app/code/local/Vinehousefarm/Birdlibrary/controllers/BirdController.php
+++ app/code/local/Vinehousefarm/Birdlibrary/controllers/BirdController.php
@@ -28,7 +28,8 @@ class Vinehousefarm_Birdlibrary_BirdController extends Mage_Core_Controller_Fron
 
             if ($bird) {
 
-                $this->getLayout()->getBlock('head')->setTitle($bird->getBirdName());
+                $this->getLayout()->getBlock('head')->setTitle($bird->getTitle());
+                $this->getLayout()->getBlock('head')->setName($bird->getTitle());
                 $this->getLayout()->getBlock('head')->setBirdName($bird->getBirdName());
                 $this->getLayout()->getBlock('head')->setDescription($bird->getDescription());
                 $this->getLayout()->getBlock('head')->setKeywords($bird->getKeywords());
